6e0d064b4940f26dfc8f17bb4b1ca880e76e753c
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-03-10 10:50:30 +0000

    added solve and added cat puns

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ get("/trivia") do
   end
   @difficulty = params.fetch("difficulty")
   if @difficulty != "Any Difficulty"
-    trivia_url = trivia_url + "&difficulty=#{@difficulty.lowercase}"
+    trivia_url = trivia_url + "&difficulty=#{@difficulty.downcase}"
   end
   
   raw_trivia_data = HTTP.get(trivia_url)
@@ -45,24 +45,16 @@ end
 
 get("/solve") do
   @cataas_url = "https://cataas.com/cat"
-  trivia_url = "https://opentdb.com/api.php?amount=1&type=multiple"
-  @category = params.fetch("category")
-  if @category != 0
-    trivia_url = trivia_url + "&category=#{}"
+  @answer = params.fetch("answer")
+  @correct_answer = params.fetch("correct_answer")
+  @reply = ""
+  if @answer == @correct_answer
+    @reply = "Purr-fect! You nailed it! 😸🎉"
+    @cataas_url = @cataas_url + "/happy"
+  else @answer == @correct_answer
+      @reply = "Oh no, that's a cat-astrophe! 😿 The correct answer is... #{@correct_answer}"
+      @cataas_url = @cataas_url + "/grumpy"
   end
-  @difficulty = params.fetch("difficulty")
-  if @difficulty != "Any Difficulty"
-    trivia_url = trivia_url + "&difficulty=#{@difficulty.lowercase}"
-  end
-  
-  raw_trivia_data = HTTP.get(trivia_url)
-  @parsed_trivia_data = JSON.parse(raw_trivia_data)
-  results = @parsed_trivia_data.fetch("results").at(0)
-  @question = results.fetch("question")
-  @correct_answer = results.fetch("correct_answer")
-  @answers = results.fetch("incorrect_answers")
-  @answers.push(@correct_answer)
-  @answers = @answers.shuffle
 
-  erb(:trivia)
+  erb(:solve)
 end
